ed8d1723fcbb83d95c28785e3fa70680e3461502
rubenaz <rubenazoulay90@gmail.com>
2019-06-25 17:44:22 +0300

    1 file modified

--- src/app/search-wheater/search-wheater.service.ts
+++ src/app/search-wheater/search-wheater.service.ts
@@ -5,7 +5,7 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 @Injectable()
 export class SearchWheaterService {
   key = "976136126d553c05d8890ac35365a99f";
-  beginUrl ="api.openweathermap.org/data/2.5/weather?q="
+  beginUrl ="https://api.openweathermap.org/data/2.5/weather?q="
   url:string;
   constructor(private http: Http) { 
   }
@@ -14,15 +14,14 @@ export class SearchWheaterService {
    getTheWheater(place)
   {
     this.url= this.beginUrl + place +"&APPID="+this.key;
-    console.log(this.url);
-    this.http.get(this.url)
-      .subscribe(data => {
-        console.log(data);
-      });
-   /*  this.http.get(this.url).toPromise().then(response => 
-    {console.log(response)
-    });*/
+    console.log(this.url)
+   this.http.get(this.url)
+    .subscribe(
+      (res: Response) => 
+        {
+          console.log(res);
+        }
+    );
+
   }
-
-
 }
