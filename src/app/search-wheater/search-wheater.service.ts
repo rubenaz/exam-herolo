911a5bdec396946d1e4ef5f851ff86e1b0c3bcb8
rubenaz <rubenazoulay90@gmail.com>
2019-06-25 22:54:39 +0300

    begin to create the table result

--- src/app/search-wheater/search-wheater.service.ts
+++ src/app/search-wheater/search-wheater.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
 import {Http,Response,HttpModule, RequestOptions, Headers,} from '@angular/http';
 import {HttpClient, HttpHeaders} from '@angular/common/http'; 
+import {formatDate} from '@angular/common';
+
 
 @Injectable()
 export class SearchWheaterService {
   MAX_DAY=5;
+  NUM_OF_DETAILS=5
   key = "976136126d553c05d8890ac35365a99f";
   beginUrl ="https://api.openweathermap.org/data/2.5/forecast?q="
   url:string;
-  wheatherList:String[][];
+  wheatherList:any[][];
 
   constructor(private http: Http) { 
   }
@@ -16,19 +19,54 @@ export class SearchWheaterService {
 
    getTheWheater(place)
   {
+    this.wheatherList= this.generate(this.wheatherList,this.MAX_DAY,this.NUM_OF_DETAILS)
+    console.log(this.wheatherList)
     let result;
+    let count=0;
   
     this.url= this.beginUrl + place +"&APPID="+this.key;
-      this.http.get(this.url).toPromise().then(response => 
-    {console.log(response)
+    console.log(this.url);
+      this.http.get(this.url).subscribe(response => 
+    {console.log(response.json())
     result=response.json();
-    for(let i =0;i<this.MAX_DAY;i++)
+    let apiDate=result.list[0].dt_txt.split(" ")[0];
+    let date = this.getDate(count)
+    let i=0;
+    this.getWheaterList(result.list[0],count)
+    count++;
+    while(count<=5)
+    {
+       apiDate=result.list[i].dt_txt.split(" ")[0];
+       date = this.getDate(count)
+      if(apiDate!=date)
       {
-          getWheaterList(result)
+            this.getWheaterList(result.list[i],count);
+            count++;
       }
+      i++;
+    }
+  
     });
 
   }
+  getWheaterList(result,count)
+  {
+      this.wheatherList[count]["date"]=2;
+  }
+  getDate(count)
+  {
+        var dte = new Date();
+    dte.setDate(dte.getDate() - count);
+    return formatDate(dte, 'yyyy-MM-dd', 'en');
+  }
+  generate(array,MAX_DAYS,NUM_OF_DETAILS)//generate the double array (data)
+    {
+        array=[];
+        for(let j=0 ; j< MAX_DAYS;j++){
+            array[j]=new array(NUM_OF_DETAILS)
+        }
+        return array
+    }
 
   
 }
