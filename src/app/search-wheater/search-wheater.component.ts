8db9487b6dccfdae00d1791486d5d37ce1bb4caa
rubenaz <rubenazoulay90@gmail.com>
2019-06-26 21:23:21 +0300

    click on the button remove remove the favorite

--- src/app/search-wheater/search-wheater.component.ts
+++ src/app/search-wheater/search-wheater.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { SearchWheaterService } from './search-wheater.service';
+import {Http,Response,HttpModule, RequestOptions, Headers,} from '@angular/http';
+import {HttpClient, HttpHeaders,HttpErrorResponse} from '@angular/common/http'; 
+import {formatDate} from '@angular/common';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -8,11 +12,77 @@ import { SearchWheaterService } from './search-wheater.service';
   styleUrls: ['./search-wheater.component.css']
 })
 export class SearchWheaterComponent implements OnInit {
+  MAX_DAY=5;
+  NUM_OF_DETAILS=5
+  key = "976136126d553c05d8890ac35365a99f";
+  beginUrl ="https://api.openweathermap.org/data/2.5/forecast?q="
+  url:string;
+  wheatherList:any[];
+  loading;
+  place;
+  placeNotExist;
+  placeEmpty
 
-  constructor(public service : SearchWheaterService) { }
 
-  ngOnInit() {
+
+
+  constructor(public service : SearchWheaterService,private http: Http) { }
+getTheWheater(place)
+  {
+    this.place="";
+    this.place=place;
+    let result;
+    let count=0;
+    this.loading=false;
+    this.placeNotExist=false;
+    //check if the input is empty
+        if(place.length==0){
+      this.placeEmpty=true;
+      return;
+     }
   
+    this.url= this.beginUrl + place +"&units=metric&APPID="+this.key;
+      this.http.get(this.url).toPromise().then(response => 
+    {console.log(response.status)
+    result=response.json();
+    this.wheatherList=this.service.generate(result.list.length)
+
+    let apiDate=result.list[0].dt_txt.split(" ")[0];
+    let date = this.service.getDate(count)
+    let i =0;
+    while(typeof result.list[i] != 'undefined')
+    {
+      apiDate=result.list[i].dt_txt.split(" ")[0]
+      date = this.service.getDate(count)
+      if(apiDate!=date)
+        count++;
+      this.wheatherList=this.service.getWheaterList(result.list[i],count,i,result["city"].id);
+      i++;
+    }
+    this.loading=true;
+    console.log(this.wheatherList);
+    }).catch((err: HttpErrorResponse) => {
+        // simple logging, but you can do a lot more, see below
+        this.placeNotExist=true;
+        console.error('An error occurred:', err);
+      });;
+  }
+
+//======================================================================================
+
+  removeFromFavorite(place)
+{
+    localStorage.removeItem(place);
+}
+  //======================================================================================
+
+addTofavorite(place)
+{
+  let myObj = { id: this.wheatherList[0].id, place: this.place, temp:this.wheatherList[0].temp,main:this.wheatherList[0].main};//this.wheatherList[0] is the current wheater
+  localStorage.setItem(this.wheatherList[0].id, JSON.stringify(myObj));
+}
+  ngOnInit() {
+  this.getTheWheater("tel aviv");
   }
 
 
--- src/app/search-wheater/search-wheater.service.ts
+++ src/app/search-wheater/search-wheater.service.ts
@@ -1,95 +1,16 @@
 import { Injectable } from '@angular/core';
-import {Http,Response,HttpModule, RequestOptions, Headers,} from '@angular/http';
-import {HttpClient, HttpHeaders,HttpErrorResponse} from '@angular/common/http'; 
 import {formatDate} from '@angular/common';
-import { Router } from '@angular/router';
+
 
 
 
 @Injectable()
 export class SearchWheaterService {
-  MAX_DAY=5;
-  NUM_OF_DETAILS=5
-  key = "976136126d553c05d8890ac35365a99f";
-  beginUrl ="https://api.openweathermap.org/data/2.5/forecast?q="
-  url:string;
-  wheatherList:any[];
-  loading;
-  place;
-  placeNotExist;
-  placeEmpty
+  rows=0;
+  wheatherList:any[]
 
-  constructor(private http: Http,private router: Router) { 
-      this.router.navigate(['/'], { queryParams: { q: 'tel aviv' } });
-    this.getTheWheater("tel aviv");
+  constructor() {
   }
-
-   getTheWheater(place)
-  {
-    this.place=place;
-    let result;
-    let count=0;
-    this.loading=false;
-    this.placeNotExist=false;
-    //check if the input is empty
-        if(place.length==0){
-      this.placeEmpty=true;
-      return;
-     }
-  
-    this.url= this.beginUrl + place +"&units=metric&APPID="+this.key;
-      this.http.get(this.url).toPromise().then(response => 
-    {console.log(response.status)
-    result=response.json();
-    this.generate(this.wheatherList,result.list.length)
-
-    let apiDate=result.list[0].dt_txt.split(" ")[0];
-    let date = this.getDate(count)
-    let i =0;
-    while(typeof result.list[i] != 'undefined')
-    {
-      apiDate=result.list[i].dt_txt.split(" ")[0]
-      date = this.getDate(count)
-      if(apiDate!=date)
-        count++;
-      this.getWheaterList(result.list[i],count,i,result["city"].id);
-      i++;
-    }
-    this.loading=true;
-    console.log(this.wheatherList);
-    }).catch((err: HttpErrorResponse) => {
-        // simple logging, but you can do a lot more, see below
-        this.placeNotExist=true;
-        console.error('An error occurred:', err);
-      });;
-  }
-
-  //======================================================================================
-
-  removeFromFavorite(place)
-{
-    localStorage.removeItem(place);
-}
-
-  //======================================================================================
-
-addTofavorite(place)
-{
-  let myObj = { id: this.wheatherList[0].id, place: this.place, temp:this.wheatherList[0].temp,main:this.wheatherList[0].main};//this.wheatherList[0] is the current wheater
-  localStorage.setItem(this.wheatherList[0].id, JSON.stringify(myObj));
-  
-
-// Read item:
-  let item = JSON.parse(localStorage.getItem(place));
-  console.log(item);
-    //localStorage.setItem(place,place);
-    //console.log(localStorage.length);
-
-}
-  
-  //======================================================================================
-  //===============================HELP FUNCTIONS=========================================
-  //======================================================================================
   getWheaterList(result,count,i,id)
   {
       this.wheatherList[i].id=id;
@@ -99,6 +20,7 @@ addTofavorite(place)
       this.wheatherList[i].temp_min=result["main"].temp_min;
       this.wheatherList[i].temp_max=result["main"].temp_max;
       this.wheatherList[i].main=result.weather[0].main;
+      return this.wheatherList
   }
   //======================================================================================
   getDate(count)
@@ -108,11 +30,13 @@ addTofavorite(place)
     return formatDate(dte, 'yyyy-MM-dd', 'en');
   }
   //======================================================================================
-  generate(array,rows,)//generate the double array (data)
+  generate(rows)//generate the double array (data)
     {
+      this.rows=rows;
         this.wheatherList=[];
         for(let j=0 ; j< rows;j++){
-            this.wheatherList[j]={id: "",num:"",date: "",temp : "", temp_min: "",temp_max:"",main:""};
+           this.wheatherList[j]={id: "",num:"",date: "",temp : "", temp_min: "",temp_max:"",main:""};
         }
+        return this.wheatherList;
     }  
 }
