59d965dad054f3835e80b6f8e23c701f80bd5207
rubenaz <rubenazoulay90@gmail.com>
2019-06-26 18:34:45 +0300

    add favorites

--- src/app/favorite-wheater/favorite-wheater.component.ts
+++ src/app/favorite-wheater/favorite-wheater.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-favorite-wheater',
   templateUrl: './favorite-wheater.component.html',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoriteWheaterComponent implements OnInit {
 
+  favorite=new Map();
+;
   constructor() { }
 
+  deleteFromFavorite(id)
+  {
+    localStorage.removeItem(id);
+    this.favorite.delete(id);
+    this.ngOnInit();
+  }
+
   ngOnInit() {
+    for (let i = 0; i < localStorage.length; i++) {
+        this.favorite.set(localStorage.key(i),JSON.parse( localStorage.getItem(localStorage.key(i))));   
+}
+        console.log(this.favorite);
+        
   }
 
 }
--- src/app/search-wheater/search-wheater.service.ts
+++ src/app/search-wheater/search-wheater.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import {Http,Response,HttpModule, RequestOptions, Headers,} from '@angular/http';
 import {HttpClient, HttpHeaders,HttpErrorResponse} from '@angular/common/http'; 
 import {formatDate} from '@angular/common';
+import { Router } from '@angular/router';
+
 
 
 @Injectable()
@@ -15,8 +17,10 @@ export class SearchWheaterService {
   loading;
   place;
   placeNotExist;
+  placeEmpty
 
-  constructor(private http: Http) { 
+  constructor(private http: Http,private router: Router) { 
+      this.router.navigate(['/'], { queryParams: { q: 'tel aviv' } });
     this.getTheWheater("tel aviv");
   }
 
@@ -27,6 +31,11 @@ export class SearchWheaterService {
     let count=0;
     this.loading=false;
     this.placeNotExist=false;
+    //check if the input is empty
+        if(place.length==0){
+      this.placeEmpty=true;
+      return;
+     }
   
     this.url= this.beginUrl + place +"&units=metric&APPID="+this.key;
       this.http.get(this.url).toPromise().then(response => 
@@ -43,16 +52,16 @@ export class SearchWheaterService {
       date = this.getDate(count)
       if(apiDate!=date)
         count++;
-      this.getWheaterList(result.list[i],count,i);
+      this.getWheaterList(result.list[i],count,i,result["city"].id);
       i++;
     }
     this.loading=true;
+    console.log(this.wheatherList);
     }).catch((err: HttpErrorResponse) => {
         // simple logging, but you can do a lot more, see below
         this.placeNotExist=true;
-        console.error('An error occurred:', err.error);
+        console.error('An error occurred:', err);
       });;
-
   }
 
   //======================================================================================
@@ -66,17 +75,25 @@ export class SearchWheaterService {
 
 addTofavorite(place)
 {
-    localStorage.setItem(place,place);
-    console.log(localStorage.length);
+  let myObj = { id: this.wheatherList[0].id, place: this.place, temp:this.wheatherList[0].temp,main:this.wheatherList[0].main};//this.wheatherList[0] is the current wheater
+  localStorage.setItem(this.wheatherList[0].id, JSON.stringify(myObj));
+  
+
+// Read item:
+  let item = JSON.parse(localStorage.getItem(place));
+  console.log(item);
+    //localStorage.setItem(place,place);
+    //console.log(localStorage.length);
 
 }
   
   //======================================================================================
   //===============================HELP FUNCTIONS=========================================
   //======================================================================================
-  getWheaterList(result,count,i)
+  getWheaterList(result,count,i,id)
   {
-      this.wheatherList[i].id=count;
+      this.wheatherList[i].id=id;
+      this.wheatherList[i].num=count;
       this.wheatherList[i].date=result.dt_txt;
       this.wheatherList[i].temp=result["main"].temp;
       this.wheatherList[i].temp_min=result["main"].temp_min;
@@ -95,7 +112,7 @@ addTofavorite(place)
     {
         this.wheatherList=[];
         for(let j=0 ; j< rows;j++){
-            this.wheatherList[j]={id:"",date: "",temp : "", temp_min: "",temp_max:"",main:""};
+            this.wheatherList[j]={id: "",num:"",date: "",temp : "", temp_min: "",temp_max:"",main:""};
         }
     }  
 }
