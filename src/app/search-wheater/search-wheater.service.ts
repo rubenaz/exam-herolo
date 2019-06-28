d734aefa2c88d56f0caf79b2b1166f62087738f5
rubenaz <rubenazoulay90@gmail.com>
2019-06-28 12:41:55 +0300

    replace listweather to map

--- src/app/search-wheater/search-wheater.service.ts
+++ src/app/search-wheater/search-wheater.service.ts
@@ -20,6 +20,7 @@ export class SearchWheaterService {
   private place:string="";
   private placeNotExist;
   private placeEmpty;
+  private id
 
   constructor(private router :Router,private http: Http) {
   }
@@ -31,7 +32,8 @@ getTheWheater(place)
     let count=0;
     this.loading=false;
     this.placeNotExist=false;
-      this.placeEmpty=false;
+    this.placeEmpty=false;
+    this.id="";
 
     //check if the input is empty
         if(place.length==0){
@@ -43,7 +45,9 @@ getTheWheater(place)
       this.http.get(this.url).toPromise().then(response => 
     {console.log(response.status)
     result=response.json();
-    this.wheatherList=this.generate(result.list.length)
+    this.id=result["city"].id;
+    this.generateTheMap(result.list);
+  /*  this.wheatherList=this.generate(result.list.length)
 
     let apiDate=result.list[0].dt_txt.split(" ")[0];
     let date = this.getDate(count)
@@ -56,7 +60,7 @@ getTheWheater(place)
         count++;
       this.wheatherList=this.getWheaterList(result.list[i],count,i,result["city"].id);
       i++;
-    }
+    }*/
     this.loading=true;
     console.log(this.wheatherList);
     }).catch((err: HttpErrorResponse) => {
@@ -73,20 +77,25 @@ getTheWheater(place)
   if(place=="")
   place=this.DEFAULT_PLACE;
     console.log(place);
-    if(localStorage.getItem(this.wheatherList[0].id) != undefined)
-    localStorage.removeItem(this.wheatherList[0].id);
+    if(localStorage.getItem(this.id) != undefined)
+    localStorage.removeItem(this.id);
 }
   //======================================================================================
 
 addTofavorite(place)
 {
    if(place=="")
-  place=this.DEFAULT_PLACE;
-  let myObj = { id: this.wheatherList[0].id, place: this.place, temp:this.wheatherList[0].temp,main:this.wheatherList[0].main};//this.wheatherList[0] is the current wheater
-  localStorage.setItem(this.wheatherList[0].id, JSON.stringify(myObj));
+    place=this.DEFAULT_PLACE;
+  let myObj = { id: this.id, place: this.place, temp:this.weatherMap.get(this.getDate())[0].temp,main:this.weatherMap.get(this.getDate())[0].main};//this.wheatherList[0] is the current wheater
+  localStorage.setItem(this.id, JSON.stringify(myObj));
 }
-
-  getWheaterList(result,count,i,id)
+getDate()
+  {
+        var dte = new Date();
+    dte.setDate(dte.getDate());
+    return formatDate(dte, 'yyyy-MM-dd', 'en');
+  }
+/*  getWheaterList(result,count,i,id)
   {
       this.wheatherList[i].id=id;
       this.wheatherList[i].num=count;
@@ -114,5 +123,30 @@ addTofavorite(place)
            this.wheatherList[j]={id: "",num:"",date: "",temp : "", temp_min: "",temp_max:"",main:""};
         }
         return this.wheatherList;
-    }  
+    }  */
+    generateTheMap(resultApi){
+        let tempArray = [];
+        let i=0;
+        let apiDate=resultApi[0].dt_txt.split(" ")[0];
+        while(typeof resultApi[i] != 'undefined')
+        {
+          let currentDate = resultApi[i].dt_txt.split(" ")[0];
+          if(apiDate!=currentDate)
+          {
+            this.weatherMap.set(apiDate,tempArray);
+            tempArray=[];
+            apiDate=currentDate;
+          }
+          tempArray.push(this.addDay(resultApi[i]))
+            i++;
+        }
+                    this.weatherMap.set(apiDate,tempArray);
+
+                    console.log(this.weatherMap);
+
+    }
+    addDay(resultApi)
+    {
+      return {date: resultApi.dt_txt,temp : resultApi["main"].temp, temp_min: resultApi["main"].temp_min,temp_max:resultApi["main"].temp_max,main:resultApi.weather[0].main}
+    }
 }
